68052bd961f1ac0874356d5814bbf49f390b4c1c
CrisLunaR <clunaarriaga20@gmail.com>
2022-05-09 14:50:50 -0600

    añadi las funciones a los botones, parte luna terminada

--- calculadora.m
+++ calculadora.m
@@ -22,7 +22,7 @@ function varargout = calculadora(varargin)
 
 % Edit the above text to modify the response to help calculadora
 
-% Last Modified by GUIDE v2.5 09-May-2022 13:59:21
+% Last Modified by GUIDE v2.5 09-May-2022 14:12:31
 
 % Begin initialization code - DO NOT EDIT
 gui_Singleton = 1;
@@ -98,6 +98,12 @@ end
 
 % --- Executes on button press in graf.
 function graf_Callback(hObject, eventdata, handles)
+axes(handles.axes1);
+val=get(handles.pantalla,'string');
+hold on
+fplot(val)
+grid
+hold on
 % hObject    handle to graf (see GCBO)
 % eventdata  reserved - to be defined in a future version of MATLAB
 % handles    structure with handles and user data (see GUIDATA)
@@ -105,6 +111,9 @@ function graf_Callback(hObject, eventdata, handles)
 
 % --- Executes on button press in punto.
 function punto_Callback(hObject, eventdata, handles)
+s=get(handles.pantalla,'string');
+s=strcat(s,'.');
+set(handles.pantalla,'string',s);
 % hObject    handle to punto (see GCBO)
 % eventdata  reserved - to be defined in a future version of MATLAB
 % handles    structure with handles and user data (see GUIDATA)
@@ -122,6 +131,9 @@ set(handles.pantalla,'string',s);
 
 % --- Executes on button press in tres.
 function tres_Callback(hObject, eventdata, handles)
+s=get(handles.pantalla,'string');
+s=strcat(s,'3');
+set(handles.pantalla,'string',s);
 % hObject    handle to tres (see GCBO)
 % eventdata  reserved - to be defined in a future version of MATLAB
 % handles    structure with handles and user data (see GUIDATA)
@@ -129,6 +141,8 @@ function tres_Callback(hObject, eventdata, handles)
 
 % --- Executes on button press in clear.
 function clear_Callback(hObject, eventdata, handles)
+s=char(' ');
+set(handles.pantalla,'string',s);
 % hObject    handle to clear (see GCBO)
 % eventdata  reserved - to be defined in a future version of MATLAB
 % handles    structure with handles and user data (see GUIDATA)
@@ -136,6 +150,9 @@ function clear_Callback(hObject, eventdata, handles)
 
 % --- Executes on button press in igual.
 function igual_Callback(hObject, eventdata, handles)
+s=get(handles.pantalla,'string');
+s=eval(s)
+set(handles.pantalla,'string',s);
 % hObject    handle to igual (see GCBO)
 % eventdata  reserved - to be defined in a future version of MATLAB
 % handles    structure with handles and user data (see GUIDATA)
@@ -143,6 +160,9 @@ function igual_Callback(hObject, eventdata, handles)
 
 % --- Executes on button press in coma.
 function coma_Callback(hObject, eventdata, handles)
+s=get(handles.pantalla,'string');
+s=strcat(s,',');
+set(handles.pantalla,'string',s);
 % hObject    handle to coma (see GCBO)
 % eventdata  reserved - to be defined in a future version of MATLAB
 % handles    structure with handles and user data (see GUIDATA)
@@ -150,6 +170,9 @@ function coma_Callback(hObject, eventdata, handles)
 
 % --- Executes on button press in dos.
 function dos_Callback(hObject, eventdata, handles)
+s=get(handles.pantalla,'string');
+s=strcat(s,'2');
+set(handles.pantalla,'string',s);
 % hObject    handle to dos (see GCBO)
 % eventdata  reserved - to be defined in a future version of MATLAB
 % handles    structure with handles and user data (see GUIDATA)
@@ -157,6 +180,9 @@ function dos_Callback(hObject, eventdata, handles)
 
 % --- Executes on button press in uno.
 function uno_Callback(hObject, eventdata, handles)
+s=get(handles.pantalla,'string');
+s=strcat(s,'1');
+set(handles.pantalla,'string',s);
 % hObject    handle to uno (see GCBO)
 % eventdata  reserved - to be defined in a future version of MATLAB
 % handles    structure with handles and user data (see GUIDATA)
@@ -164,6 +190,9 @@ function uno_Callback(hObject, eventdata, handles)
 
 % --- Executes on button press in seis.
 function seis_Callback(hObject, eventdata, handles)
+s=get(handles.pantalla,'string');
+s=strcat(s,'6');
+set(handles.pantalla,'string',s);
 % hObject    handle to seis (see GCBO)
 % eventdata  reserved - to be defined in a future version of MATLAB
 % handles    structure with handles and user data (see GUIDATA)
@@ -171,6 +200,9 @@ function seis_Callback(hObject, eventdata, handles)
 
 % --- Executes on button press in cinco.
 function cinco_Callback(hObject, eventdata, handles)
+s=get(handles.pantalla,'string');
+s=strcat(s,'5');
+set(handles.pantalla,'string',s);
 % hObject    handle to cinco (see GCBO)
 % eventdata  reserved - to be defined in a future version of MATLAB
 % handles    structure with handles and user data (see GUIDATA)
@@ -178,6 +210,9 @@ function cinco_Callback(hObject, eventdata, handles)
 
 % --- Executes on button press in cuatro.
 function cuatro_Callback(hObject, eventdata, handles)
+s=get(handles.pantalla,'string');
+s=strcat(s,'4');
+set(handles.pantalla,'string',s);
 % hObject    handle to cuatro (see GCBO)
 % eventdata  reserved - to be defined in a future version of MATLAB
 % handles    structure with handles and user data (see GUIDATA)
@@ -185,6 +220,9 @@ function cuatro_Callback(hObject, eventdata, handles)
 
 % --- Executes on button press in nueve.
 function nueve_Callback(hObject, eventdata, handles)
+s=get(handles.pantalla,'string');
+s=strcat(s,'9');
+set(handles.pantalla,'string',s);
 % hObject    handle to nueve (see GCBO)
 % eventdata  reserved - to be defined in a future version of MATLAB
 % handles    structure with handles and user data (see GUIDATA)
@@ -192,6 +230,9 @@ function nueve_Callback(hObject, eventdata, handles)
 
 % --- Executes on button press in ocho.
 function ocho_Callback(hObject, eventdata, handles)
+s=get(handles.pantalla,'string');
+s=strcat(s,'8');
+set(handles.pantalla,'string',s);
 % hObject    handle to ocho (see GCBO)
 % eventdata  reserved - to be defined in a future version of MATLAB
 % handles    structure with handles and user data (see GUIDATA)
@@ -199,6 +240,9 @@ function ocho_Callback(hObject, eventdata, handles)
 
 % --- Executes on button press in siete.
 function siete_Callback(hObject, eventdata, handles)
+s=get(handles.pantalla,'string');
+s=strcat(s,'7');
+set(handles.pantalla,'string',s);
 % hObject    handle to siete (see GCBO)
 % eventdata  reserved - to be defined in a future version of MATLAB
 % handles    structure with handles and user data (see GUIDATA)
@@ -206,6 +250,9 @@ function siete_Callback(hObject, eventdata, handles)
 
 % --- Executes on button press in mas.
 function mas_Callback(hObject, eventdata, handles)
+s=get(handles.pantalla,'string');
+s=strcat(s,'+');
+set(handles.pantalla,'string',s);
 % hObject    handle to mas (see GCBO)
 % eventdata  reserved - to be defined in a future version of MATLAB
 % handles    structure with handles and user data (see GUIDATA)
@@ -220,6 +267,9 @@ function por_Callback(hObject, eventdata, handles)
 
 % --- Executes on button press in menos.
 function menos_Callback(hObject, eventdata, handles)
+s=get(handles.pantalla,'string');
+s=strcat(s,'-');
+set(handles.pantalla,'string',s);
 % hObject    handle to menos (see GCBO)
 % eventdata  reserved - to be defined in a future version of MATLAB
 % handles    structure with handles and user data (see GUIDATA)
@@ -234,6 +284,9 @@ function div_Callback(hObject, eventdata, handles)
 
 % --- Executes on button press in equis.
 function equis_Callback(hObject, eventdata, handles)
+s=get(handles.pantalla,'string');
+s=strcat(s,'x');
+set(handles.pantalla,'string',s);
 % hObject    handle to equis (see GCBO)
 % eventdata  reserved - to be defined in a future version of MATLAB
 % handles    structure with handles and user data (see GUIDATA)
@@ -241,6 +294,9 @@ function equis_Callback(hObject, eventdata, handles)
 
 % --- Executes on button press in raiz2.
 function raiz2_Callback(hObject, eventdata, handles)
+s=get(handles.pantalla,'string');
+s=strcat(s,'sqrt(');
+set(handles.pantalla,'string',s);
 % hObject    handle to raiz2 (see GCBO)
 % eventdata  reserved - to be defined in a future version of MATLAB
 % handles    structure with handles and user data (see GUIDATA)
@@ -248,6 +304,9 @@ function raiz2_Callback(hObject, eventdata, handles)
 
 % --- Executes on button press in raiz3.
 function raiz3_Callback(hObject, eventdata, handles)
+s=get(handles.pantalla,'string');
+s=strcat(s,'nthroot(',s,',3)');
+set(handles.pantalla,'string',s);
 % hObject    handle to raiz3 (see GCBO)
 % eventdata  reserved - to be defined in a future version of MATLAB
 % handles    structure with handles and user data (see GUIDATA)
@@ -255,6 +314,9 @@ function raiz3_Callback(hObject, eventdata, handles)
 
 % --- Executes on button press in elev3.
 function elev3_Callback(hObject, eventdata, handles)
+s=get(handles.pantalla,'string');
+s=strcat(s,'^3');
+set(handles.pantalla,'string',s);
 % hObject    handle to elev3 (see GCBO)
 % eventdata  reserved - to be defined in a future version of MATLAB
 % handles    structure with handles and user data (see GUIDATA)
@@ -262,6 +324,9 @@ function elev3_Callback(hObject, eventdata, handles)
 
 % --- Executes on button press in in.
 function in_Callback(hObject, eventdata, handles)
+s=get(handles.pantalla,'string');
+s=strcat(s,'log(');
+set(handles.pantalla,'string',s);
 % hObject    handle to in (see GCBO)
 % eventdata  reserved - to be defined in a future version of MATLAB
 % handles    structure with handles and user data (see GUIDATA)
@@ -269,6 +334,9 @@ function in_Callback(hObject, eventdata, handles)
 
 % --- Executes on button press in log.
 function log_Callback(hObject, eventdata, handles)
+s=get(handles.pantalla,'string');
+s=strcat(s,'log10(');
+set(handles.pantalla,'string',s);
 % hObject    handle to log (see GCBO)
 % eventdata  reserved - to be defined in a future version of MATLAB
 % handles    structure with handles and user data (see GUIDATA)
@@ -276,6 +344,9 @@ function log_Callback(hObject, eventdata, handles)
 
 % --- Executes on button press in raiz.
 function raiz_Callback(hObject, eventdata, handles)
+s=get(handles.pantalla,'string');
+s=strcat(s,'nthroot(');
+set(handles.pantalla,'string',s);
 % hObject    handle to raiz (see GCBO)
 % eventdata  reserved - to be defined in a future version of MATLAB
 % handles    structure with handles and user data (see GUIDATA)
@@ -283,6 +354,9 @@ function raiz_Callback(hObject, eventdata, handles)
 
 % --- Executes on button press in elev2.
 function elev2_Callback(hObject, eventdata, handles)
+s=get(handles.pantalla,'string');
+s=strcat(s,'^2');
+set(handles.pantalla,'string',s);
 % hObject    handle to elev2 (see GCBO)
 % eventdata  reserved - to be defined in a future version of MATLAB
 % handles    structure with handles and user data (see GUIDATA)
@@ -290,6 +364,9 @@ function elev2_Callback(hObject, eventdata, handles)
 
 % --- Executes on button press in seno.
 function seno_Callback(hObject, eventdata, handles)
+s=get(handles.pantalla,'string');
+s=strcat(s,'sin(');
+set(handles.pantalla,'string',s);
 % hObject    handle to seno (see GCBO)
 % eventdata  reserved - to be defined in a future version of MATLAB
 % handles    structure with handles and user data (see GUIDATA)
@@ -297,6 +374,9 @@ function seno_Callback(hObject, eventdata, handles)
 
 % --- Executes on button press in coseno.
 function coseno_Callback(hObject, eventdata, handles)
+s=get(handles.pantalla,'string');
+s=strcat(s,'cos(');
+set(handles.pantalla,'string',s);
 % hObject    handle to coseno (see GCBO)
 % eventdata  reserved - to be defined in a future version of MATLAB
 % handles    structure with handles and user data (see GUIDATA)
@@ -304,6 +384,9 @@ function coseno_Callback(hObject, eventdata, handles)
 
 % --- Executes on button press in tangente.
 function tangente_Callback(hObject, eventdata, handles)
+s=get(handles.pantalla,'string');
+s=strcat(s,'tan(');
+set(handles.pantalla,'string',s);
 % hObject    handle to tangente (see GCBO)
 % eventdata  reserved - to be defined in a future version of MATLAB
 % handles    structure with handles and user data (see GUIDATA)
@@ -311,6 +394,9 @@ function tangente_Callback(hObject, eventdata, handles)
 
 % --- Executes on button press in elevado.
 function elevado_Callback(hObject, eventdata, handles)
+s=get(handles.pantalla,'string');
+s=strcat(s,'^');
+set(handles.pantalla,'string',s);
 % hObject    handle to elevado (see GCBO)
 % eventdata  reserved - to be defined in a future version of MATLAB
 % handles    structure with handles and user data (see GUIDATA)
@@ -318,6 +404,9 @@ function elevado_Callback(hObject, eventdata, handles)
 
 % --- Executes on button press in parentesisIzq.
 function parentesisIzq_Callback(hObject, eventdata, handles)
+s=get(handles.pantalla,'string');
+s=strcat(s,'(');
+set(handles.pantalla,'string',s);
 % hObject    handle to parentesisIzq (see GCBO)
 % eventdata  reserved - to be defined in a future version of MATLAB
 % handles    structure with handles and user data (see GUIDATA)
@@ -325,6 +414,9 @@ function parentesisIzq_Callback(hObject, eventdata, handles)
 
 % --- Executes on button press in parentesisDer.
 function parentesisDer_Callback(hObject, eventdata, handles)
+s=get(handles.pantalla,'string');
+s=strcat(s,')');
+set(handles.pantalla,'string',s);
 % hObject    handle to parentesisDer (see GCBO)
 % eventdata  reserved - to be defined in a future version of MATLAB
 % handles    structure with handles and user data (see GUIDATA)
@@ -332,6 +424,16 @@ function parentesisDer_Callback(hObject, eventdata, handles)
 
 % --- Executes on button press in pi.
 function pi_Callback(hObject, eventdata, handles)
+s=get(handles.pantalla,'string');
+s=strcat(s,'pi');
+set(handles.pantalla,'string',s);
 % hObject    handle to pi (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+
+
+% --- Executes on button press in tangente.
+function pushbutton35_Callback(hObject, eventdata, handles)
+% hObject    handle to tangente (see GCBO)
 % eventdata  reserved - to be defined in a future version of MATLAB
 % handles    structure with handles and user data (see GUIDATA)
